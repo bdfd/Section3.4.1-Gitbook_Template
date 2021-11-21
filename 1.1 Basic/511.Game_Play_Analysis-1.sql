bc139eaca81f3a86961a4e626813cfa2f6ddde55
BDFD <bdfd2005@gmail.com>
2021-11-21 01:39:10 -0500

    Update Question 511

--- 1.1 Basic/511.Game_Play_Analysis-1.sql
+++ 1.1 Basic/511.Game_Play_Analysis-1.sql
@@ -37,3 +37,31 @@
 -- | 2         | 2017-06-25  |
 -- | 3         | 2016-03-02  |
 -- +-----------+-------------+
+
+CREATE TABLE Activity (
+  `player_id` INTEGER,
+  `device_id` INTEGER,
+  `event_date` DATETIME,
+  `games_played` INTEGER
+);
+
+INSERT INTO Activity
+  (`player_id`, `device_id`, `event_date`, `games_played`)
+VALUES
+  ('1', '2', '2016-03-01', '5'),
+  ('1', '2', '2016-05-02', '6'),
+  ('2', '3', '2017-06-25', '1'),
+  ('3', '1', '2016-03-02', '0'),
+  ('3', '4', '2018-07-03', '5');
+
+CREATE TABLE Result (
+  `player_id` INTEGER,
+  `first_login` DATETIME
+);
+
+INSERT INTO Result
+  (`player_id`, `first_login`)
+VALUES
+  ('1', '2016-03-01'),
+  ('2', '2017-06-25'),
+  ('3', '2016-03-02');
