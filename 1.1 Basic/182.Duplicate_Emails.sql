7767d6212000dd250de25bf68164a45518e8eee8
BDFD <bdfd2005@gmail.com>
2021-11-21 01:13:05 -0500

    Update Question 182

--- 1.1 Basic/182.Duplicate_Emails.sql
+++ 1.1 Basic/182.Duplicate_Emails.sql
@@ -6,7 +6,7 @@
 --  * @FilePath: \1.3_Leetcode_SQL_All_Solution\1.1 Basic\1.html
 
 
--- Question 11
+-- Question 182
 -- Write a SQL query to find all duplicate emails in a table named Person.
 
 -- +----+---------+
@@ -23,3 +23,25 @@
 -- +---------+
 -- | a@b.com |
 -- +---------+
+
+CREATE TABLE Person (
+  `Id` INTEGER,
+  `Email` VARCHAR(7)
+);
+
+INSERT INTO Person
+  (`Id`, `Email`)
+VALUES
+  ('1', 'a@b.com'),
+  ('2', 'c@d.com'),
+  ('3', 'a@b.com');
+
+CREATE TABLE Result (
+  `Email` VARCHAR(7),
+  `ignore` VARCHAR(1)
+);
+
+INSERT INTO Result
+  (`Email`, `ignore`)
+VALUES
+  ('a@b.com', '_');
